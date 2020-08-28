118895c07515522759deebd9b381acb78c9899ab
Matt Heslington <matt.heslington@icloud.com>
2020-08-28 16:47:53 +0800

    Add upload functionality

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { useStorage } from '../hooks/useStorage';
+import useStorage from '../hooks/useStorage';
 
 const ProgressBar = ({ file, setFile}) => {
 
     const { url, progress } = useStorage(file);
-    console.log(progress, url)
 
     return (
-        <div className="progress-bar">
-            progress
-        </div>
+        <div className="progress-bar" style={{ width: progress + '%' }}></div>
     )
 }
 
-export default ProgressBar
+export default ProgressBar;
